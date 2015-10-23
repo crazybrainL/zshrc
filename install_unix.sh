65fda3626eb8b3a75ee6245402775ef0ed186f28
whlin <whlin626@gmail.com>
2015-10-23 14:53:58 +0800

    install_unix.sh zsh-theme setting

--- install_unix.sh
+++ install_unix.sh
@@ -4,4 +4,6 @@
 rm ~/.zshrc
 ln -s `pwd`/zshrc ~/.zshrc 
 ln -s ~/.oh-my-zsh `pwd`/oh-my-zsh
-cp ./whlin.zsh-theme ./oh-my-zsh/themes/
+ln -s `pwd`/whlin.zsh-theme `pwd`/oh-my-zsh/themes/whlin.zsh-theme
+
+#cp ./whlin.zsh-theme ./oh-my-zsh/themes/
