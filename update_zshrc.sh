9b71890fcb3d60cdb6cd2295e5ddd3f91fcf09f9
whlin <whlin626@gmail.com>
2015-10-24 06:46:03 +0800

    update_zshrc.sh update

--- update_zshrc.sh
+++ update_zshrc.sh
@@ -1,5 +1,5 @@
 cp ./zshrc $RCPATH/zshrc
-cp ./whlin.zsh-theme $RCPATH/whlin.zsh-theme
+cp ./whlin.zsh-theme $RCPATH/zshrc
 
 #cp ./zshrc ~/rc_files/zshrc
 #cp ./whlin.zsh-theme ~/rc_files/whlin.zsh-theme
