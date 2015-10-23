b279814c3c7c08ae16f7b6d8d7b5ce441de0bf80
whlin <whlin626@gmail.com>
2015-10-24 02:37:36 +0800

    install_unix.sh update

--- install_unix.sh
+++ install_unix.sh
@@ -2,8 +2,11 @@
 
 ## Link dictionay and files
 rm ~/.zshrc
+rm ~/.oh-my-zsh/themes/whlin.zsh-theme
+
 ln -s `pwd`/zshrc ~/.zshrc 
 ln -s ~/.oh-my-zsh `pwd`/oh-my-zsh
 ln -s `pwd`/whlin.zsh-theme `pwd`/oh-my-zsh/themes/whlin.zsh-theme
+
 
 #cp ./whlin.zsh-theme ./oh-my-zsh/themes/
