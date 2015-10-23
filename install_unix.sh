1a3c86875fe913b2d15ea02fd76813b6e2a1668d
whlin <whlin626@gmail.com>
2015-10-23 13:37:41 +0800

    zshrc 20151023 update

--- install_unix.sh
+++ install_unix.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+## install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 ## Link dictionay and files
 rm ~/.zshrc
 ln -s `pwd`/zshrc ~/.zshrc 
