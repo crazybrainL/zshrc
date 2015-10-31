27fcb21292580e42da172ee950b9ce24ad41043d
whlin <whlin626@gmail.com>
2015-10-31 19:23:43 +0800

    remove update_zshrc.sh

--- update_zshrc.sh
+++
@@ -1,7 +0,0 @@
-cp ./zshrc $RCPATH/zshrc
-cp ./whlin.zsh-theme $RCPATH/zshrc
-
-echo update zshrc/zshrc to local rc_files
-echo update zshrc/whlin.zsh-theme to local rc_files
-#cp ./zshrc ~/rc_files/zshrc
-#cp ./whlin.zsh-theme ~/rc_files/whlin.zsh-theme
